1dec8a1f56961af6ae6c49e846e815a6b16591f4
JustinTheeDude <JustinTheeDude@gmail.com>
2018-09-04 21:02:56 -0700

    asdf

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import Search from './components/search';
 import axios from 'axios';
+import {Typeahead} from 'react-bootstrap-typeahead';
+
 
 class App extends Component {
   state = {
     cards: []
   }
 
+var Typeahead = require('react-bootstrap-typeahead').Typeahead;
+
 componentDidMount() {
   axios.get('https://api.magicthegathering.io/v1/cards')
-    .then(res => { 
+    .then(res => {
         const cards = res.data;
         this.setState(cards: cards);
     });
